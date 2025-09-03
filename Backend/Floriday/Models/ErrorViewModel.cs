92fa9302d82b37e2ca6a47dc8bb0329d1da21f01
Waylo2016 <wellinkstijn@gmail.com>
2025-09-03 10:50:26 +0200

    added csproj
    added ASP.NET program into the git repo. May need to rework the docker tho.. again

---
+++ Backend/Floriday/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Floriday.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
