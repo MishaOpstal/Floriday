eecabe3b73ff8efe3b5b9d34930083b570c04aaa
Misha Opstal <52963380+MishaOpstal@users.noreply.github.com>
2025-10-01 10:06:06 +0200

    Delete Backend directory
    NUKE NUKE NUKE

--- Backend/Floriday/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Floriday.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
