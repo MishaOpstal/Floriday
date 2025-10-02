4dd9fdc73a87e3ea99eb6c9288556d6c930ffbb6
Waylo2016 <wellinkstijn@gmail.com>
2025-10-02 11:59:36 +0200

    added more controllers
    Added Auction Sales Controller, AuctionSalesProducts and Buyer Controller. Maybe we should add delete functionality to BuyerController

---
+++ LeafBidAPI/Controllers/AuctionSalesController.cs
@@ -0,0 +1,36 @@
+﻿using LeafBidAPI.Data;
+using LeafBidAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeafBidAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionSalesController(ApplicationDbContext dbContext) : BaseController(dbContext)
+{
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionSales>>> GetAuctionSales()
+    {
+        return await DbContext.AuctionSales.ToListAsync();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AuctionSales>> GetAuctionSale(int id)
+    {
+        var auctionSale = await DbContext.AuctionSales.FindAsync(id);
+        if (auctionSale == null)
+            return NotFound();
+
+        return auctionSale;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionSales>> CreateAuctionSale(AuctionSales auctionSale)
+    {
+        DbContext.AuctionSales.Add(auctionSale);
+        await DbContext.SaveChangesAsync();
+
+        return new JsonResult(auctionSale) { StatusCode = 201 };
+    }
+}
---
+++ LeafBidAPI/Controllers/AuctionSalesProductsController.cs
@@ -0,0 +1,53 @@
+﻿using LeafBidAPI.Data;
+using LeafBidAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeafBidAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionSalesProductController(ApplicationDbContext dbContext) : BaseController(dbContext)
+{
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionSalesProducts>>> GetAuctionSalesProducts()
+    {
+        return await DbContext.AuctionSalesProducts.ToListAsync();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AuctionSalesProducts>> GetAuctionSalesProduct(int id)
+    {
+        var auctionSaleProduct = await DbContext.AuctionSalesProducts.FindAsync(id);
+        if (auctionSaleProduct == null)
+            return NotFound();
+
+        return auctionSaleProduct;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionSalesProducts>> CreateAuctionSalesProducts(AuctionSalesProducts auctionSalesProduct)
+    {
+        DbContext.AuctionSalesProducts.Add(auctionSalesProduct);
+        await DbContext.SaveChangesAsync();
+
+        return new JsonResult(auctionSalesProduct) { StatusCode = 201 };
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult<AuctionSalesProducts>> UpdateAuctionSalesProducts(
+        int id, AuctionSalesProducts auctionSalesProduct)
+    {
+        var AuctionSalesProducts = await DbContext.AuctionSalesProducts.FindAsync(id);
+        if (AuctionSalesProducts == null)
+            return NotFound();
+
+        AuctionSalesProducts.AuctionSale = auctionSalesProduct.AuctionSale;
+        AuctionSalesProducts.Product = auctionSalesProduct.Product;
+        AuctionSalesProducts.Quantity = auctionSalesProduct.Quantity;
+        AuctionSalesProducts.Price = auctionSalesProduct.Price;
+        
+        await DbContext.SaveChangesAsync();
+        return new JsonResult(AuctionSalesProducts);
+    }
+}
