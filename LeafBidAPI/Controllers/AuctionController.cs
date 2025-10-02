25ecda006b3b47e68ae8d5e3e5a22c8642d191c7
Misha Opstal <mishaopstal2004@gmail.com>
2025-10-02 10:21:48 +0200

    Finished a basic example controller that should be used as a template for future controllers

--- LeafBidAPI/Controllers/AuctionController.cs
+++ LeafBidAPI/Controllers/AuctionController.cs
@@ -24,4 +24,30 @@ public class AuctionController(ApplicationDbContext dbContext) : BaseController(
 
         return auction;
     }
+
+    [HttpPost]
+    public async Task<ActionResult<Auction>> CreateAuction(Auction auction)
+    {
+        DbContext.Auctions.Add(auction);
+        await DbContext.SaveChangesAsync();
+
+        return new JsonResult(auction) { StatusCode = 201 };
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateAuction(int id, Auction updatedAuction)
+    {
+        var auction = await DbContext.Auctions.FindAsync(id);
+        if (auction == null)
+            return NotFound();
+
+        auction.Description = updatedAuction.Description;
+        auction.StartDate = updatedAuction.StartDate;
+        auction.Amount = updatedAuction.Amount;
+        auction.MinimumPrice = updatedAuction.MinimumPrice;
+        auction.ClockLocationEnum = updatedAuction.ClockLocationEnum;
+        auction.Auctioneer = updatedAuction.Auctioneer;
+        await DbContext.SaveChangesAsync();
+        return new JsonResult(auction);
+    }
 }
