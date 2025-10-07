c905e51ff421cd35e2a60a2701edff8dfcfd3366
b4djaan <daanbroek05@gmail.com>
2025-10-07 14:21:15 +0200

    XML descriptions buyers

--- LeafBidAPI/Models/Buyer.cs
+++ LeafBidAPI/Models/Buyer.cs
@@ -1,8 +1,19 @@
 ﻿namespace LeafBidAPI.Models;
 
+/// <summary>
+/// Represents a buyer in the system.
+/// </summary>
 public class Buyer
 {
+    /// <summary>
+    /// Unique identifier for the buyer.
+    /// </summary>
     public int Id { get; set; }
+    
     public User User { get; set; }
+    
+    /// <summary>
+    /// Name of the buyer's company.
+    /// </summary>
     public string CompanyName { get; set; }
 }
