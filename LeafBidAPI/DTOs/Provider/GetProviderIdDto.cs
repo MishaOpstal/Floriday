178a8391fdcabf500170b9aead45467a4e5ac979
Robin <robinrid6@gmail.com>
2025-10-11 18:14:58 +0200

    Add Data Transfer Objects for provider creation, deletion, retrieval, and updates

---
+++ LeafBidAPI/DTOs/Provider/GetProviderIdDto.cs
@@ -0,0 +1,10 @@
+namespace LeafBidAPI.DTOs.Provider;
+
+public class GetProviderIdDto
+{
+    /// <summary>
+    /// Data required to get a provider by ID
+    /// </summary>
+    
+    public required int Id { get; set; }
+}
