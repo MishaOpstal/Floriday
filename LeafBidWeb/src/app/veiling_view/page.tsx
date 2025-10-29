6ac2b925611f4b00e06266e0ccdc03988e97a249
Pootsvis <nickpoots2@gmail.com>
2025-10-29 16:49:43 +0100

    begin gemaakt aan het grote info veld voor de veiling view pagina

--- LeafBidWeb/src/app/veiling_view/page.tsx
+++ LeafBidWeb/src/app/veiling_view/page.tsx
@@ -1,9 +1,11 @@
 ﻿import InfoVeld from "@/components/infoVeldKlein/infoVeldKlein";
+import BigInfoVeld from "@/components/veilingInfo/veilingInfo";
 
 export default function Profile() {
     return (
         <main>
             {/*<InfoVeld naam="Naam" prijs={10000} plaatje="bloem.png"/>*/}
+            <BigInfoVeld naam="Naam" prijs={10000} plaatje="bloem.png" duur="1 uur" info="epic bloemen" oogst="10 april" leverancier="pieter" regio="zuid holland" aantal={100}/>
 
         </main>
     );
