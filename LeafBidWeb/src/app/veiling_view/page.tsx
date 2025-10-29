84b97cc199187137d0fbeba1ce71f088f9c34d38
Pootsvis <nickpoots2@gmail.com>
2025-10-29 14:39:18 +0100

    Add InfoVeldKlein component and veiling_view page
    begonnen met invulveld component werkt nog niet

---
+++ LeafBidWeb/src/app/veiling_view/page.tsx
@@ -0,0 +1,11 @@
+﻿import InfoVeld from "@/components/infoVeldKlein/infoVeldKlein";
+
+export default function Profile() {
+    return (
+        <main>
+            <InfoVeld label="Naam" value="Nick poots" />
+            <InfoVeld label="E-mail" value="nick@example.com" />
+            <InfoVeld label="Locatie" value="Den Haag, Nederland" />
+        </main>
+    );
+}
---
+++ LeafBidWeb/src/components/infoVeldKlein/infoVeldKlein.tsx
@@ -0,0 +1,15 @@
+﻿import s from './infoVeldKlein.module.css';
+
+type InfoFieldProps = {
+    label: string;
+    value: string | number;
+};
+
+export default function InfoVeld({ label, value }: InfoFieldProps) {
+    return (
+        <div className={s.wrapper}>
+            <span className={s.label}>{label}</span>
+            <span className={s.value}>{value}</span>
+        </div>
+    );
+}
