1777c9e4849d95bbf6d5b987933d5e5b50859e1a
Waylo2016 <wellinkstijn@gmail.com>
2025-10-29 10:41:18 +0100

    refactored placements of files
    did some changing of the file placements

---
+++ LeafBidWeb/src/app/Auth/page.tsx
@@ -0,0 +1,6 @@
+import Head from "next/head";
+import Header from "@/components/header/header";
+import Login from "@/components/LoginForm/LoginForm.module.css";
+import Register from "@/components/RegisterForm/RegisterForm.module.css";
+import s from  "@/app/Auth/page.module.css"
+
