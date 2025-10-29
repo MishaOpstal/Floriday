4e3aff529ae3b5e9c4496dde84f14bd2e04e152a
Waylo2016 <wellinkstijn@gmail.com>
2025-10-29 12:04:25 +0100

    starting login page
    starting the login page, got something to show, at least

--- LeafBidWeb/src/app/Auth/page.tsx
+++ LeafBidWeb/src/app/Auth/page.tsx
@@ -2,21 +2,22 @@ import Head from "next/head";
 import Header from "@/components/header/header";
 import LoginS from "@/components/LoginForm/LoginForm.module.css";
 import RegisterS from "@/components/RegisterForm/RegisterForm.module.css";
-import s from  "@/app/Auth/page.module.css"
 
+import s from  "@/app/Auth/page.module.css"
 import React from "react";
+import Image from "next/image";
 
 export default function Login(){
-    const [login, setLogin] = React.useState(true);
+    return(
 
-    function handleChange(event: React.ChangeEvent<HTMLInputElement>){
-        setLogin(event.currentTarget.value === "login");
-    }
+        <div className={s.startFrame}>
+            <div className={s.frame}>
+                <div className={s.image}>
+                </div>
+                <div className={s.loginRegister}>
 
-    return (
-      <>
-        <input value={login} onChange={handleChange} />
-          <p>Value: {login}</p>
-      </>
+                </div>
+            </div>
+        </div>
     );
 }
