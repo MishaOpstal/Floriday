cead580399c729c7233eb63e9b1361740aa2d96e
Misha Opstal <52963380+MishaOpstal@users.noreply.github.com>
2025-10-29 21:25:46 +0100

    Fix indentation

--- LeafBidWeb/src/app/toevoegen/layout.tsx
+++ LeafBidWeb/src/app/toevoegen/layout.tsx
@@ -3,14 +3,14 @@ import s from './layout.module.css';
 import React from "react";
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <>
-            <Header />
-            <main className={s.main}>{children}</main>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main className={s.main}>{children}</main>
+    </>
+  );
 }
