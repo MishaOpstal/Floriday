166a710a5381e934487298b3155bd9d903203d7e
Waylo2016 <wellinkstijn@gmail.com>
2025-10-30 12:53:00 +0100

    fixed css for login
    fixed the css for login, also changed the input fields to align with what we have in the register form

--- LeafBidWeb/src/components/LoginForm/LoginForm.tsx
+++ LeafBidWeb/src/components/LoginForm/LoginForm.tsx
@@ -5,14 +5,13 @@ export default function LoginForm() {
     return (
         <div className={s.container}>
             <div className={s.underline}>
-                <label className={s.authitem}>Email Adres:</label>
-                <input className={s.input} type="email" id="Email" required />
+                <label className={`${s.authitem} ${s.srOnly}`} htmlFor="Email">Email Adres:</label>
+                <input className={s.input} type="email" id="Email" placeholder="Email Adres" required />
             </div>
             <div className={s.underline}>
-                <label className={s.authitem}>Wachtwoord:</label>
-                <input className={s.input} type="password" id="Password" required />
+                <label className={`${s.authitem} ${s.srOnly}`} htmlFor="Password">Wachtwoord:</label>
+                <input className={s.input} type="password" id="Password" placeholder="Wachtwoord" required />
             </div>
-
         </div>
     );
 }
