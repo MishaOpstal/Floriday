32e1cdd85ffad03281698fc3bfa585c2efebf88b
Waylo2016 <wellinkstijn@gmail.com>
2025-10-30 12:37:06 +0100

    fixed lining of input fields & tweaked input fields
    fixed the lining of input fields  & tweaked the input fields to be the entire length of the underlining

--- LeafBidWeb/src/components/RegisterForm/RegisterForm.tsx
+++ LeafBidWeb/src/components/RegisterForm/RegisterForm.tsx
@@ -5,21 +5,21 @@ export default function RegisterForm() {
     return (
         <div className={s.container}>
             <div className={s.underline}>
-                <label className={s.authitem}>Naam:</label>
-                <input className={s.input} type="text" id="name" required />
+                <label className={`${s.authitem} ${s.srOnly}`} htmlFor="name">Naam:</label>
+                <input className={s.input} type="text" id="name" placeholder="Naam" required />
             </div>
             <div className={s.underline}>
-                <label className={s.authitem}>Email Adres:</label>
-                <input className={s.input} type="email" id="Email" required />
+                <label className={`${s.authitem} ${s.srOnly}`} htmlFor="Email">Email Adres:</label>
+                <input className={s.input} type="email" id="Email" placeholder="Email Adres" required />
             </div>
             <div className={s.underline}>
-                <label className={s.authitem}>Wachtwoord:</label>
-                <input className={s.input} type="password" id="Password" required />
+                <label className={`${s.authitem} ${s.srOnly}`} htmlFor="Password">Wachtwoord:</label>
+                <input className={s.input} type="password" id="Password" placeholder="Wachtwoord" required />
             </div>
 
             <div className={s.underline}>
-                <label className={s.authitem}>Wachtwoord:</label>
-                <input className={s.input} type="password" id="PasswordCheck" required />
+                <label className={`${s.authitem} ${s.srOnly}`} htmlFor="PasswordCheck">Wachtwoord:</label>
+                <input className={s.input} type="password" id="PasswordCheck" placeholder="Wachtwoord" required />
             </div>
         </div>
     );
