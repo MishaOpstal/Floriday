2143c24b933067fd72b7c2aa8ae8d09229e70a58
b4djaan <daanbroek05@gmail.com>
2025-11-12 23:55:47 +0100

    UPDATE: 😭 veilingmeesteroverzicht uit de api

---
+++ LeafBidAPI/DTOs/Page/GetPageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LeafBidAPI.DTOs.Page;
+using LeafBidAPI.Models;
+
+public class GetPageDto
+{
+    public required Auction auction { get; set; }
+
+    public required Product product { get; set; }
+
+}
