cbfd53c59eda3856528fb846107afe48371ee0e0
Robin <robinrid6@gmail.com>
2025-11-25 16:43:42 +0100

    REFINE: enhance layout responsiveness and improve DateSelect component styling
    Reviewed-by: DAAN

--- LeafBidWeb/src/app/layout.tsx
+++ LeafBidWeb/src/app/layout.tsx
@@ -3,18 +3,19 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
     title: "LeafBid",
     description: "Veilingen voor jou favoriete groene producten",
+
     icons: {
         icon: [
             { url: "/icons/favicon.ico" },
@@ -22,6 +23,7 @@ export const metadata: Metadata = {
             { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
             { url: "/icons/favicon-48x48.png", sizes: "48x48", type: "image/png" }
         ],
+
         apple: [
             { url: "/icons/apple-touch-icon-60x60.png", sizes: "60x60" },
             { url: "/icons/apple-touch-icon-72x72.png", sizes: "72x72" },
@@ -29,24 +31,48 @@ export const metadata: Metadata = {
             { url: "/icons/apple-touch-icon-152x152.png", sizes: "152x152" },
             { url: "/icons/apple-touch-icon-180x180.png", sizes: "180x180" }
         ],
+
         other: [
-            { rel: "icon", url: "/icons/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-            { rel: "icon", url: "/icons/android-chrome-512x512.png", sizes: "512x512", type: "image/png" }
+            {
+                rel: "icon",
+                url: "/icons/android-chrome-192x192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                rel: "icon",
+                url: "/icons/android-chrome-512x512.png",
+                sizes: "512x512",
+                type: "image/png"
+            }
         ]
     },
+
     manifest: "/icons/site.webmanifest",
+
     other: {
         "msapplication-TileColor": "#da532c",
         "msapplication-TileImage": "/icons/mstile-150x150.png"
     }
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="nl-NL" data-theme="light">
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-        </body>
-        </html>
-    );
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="nl-NL" data-theme="light">
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+              rel="stylesheet"/>
+    </head>
+    <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    {children}
+    </body>
+    </html>
+  );
 }
